721fa75fb1410258b68dc9750bd96ebc4e3890c8
Paloma Maquez <maquez.paloma@gmail.com>
2022-02-26 10:42:44 -0300

    isskkjs

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,19 @@
-const form = document.querySelector('.contact_form');
-const name = document.querySelector('.name');
-const email = document.querySelector('.email');
-const asunto = document.querySelector('.asunto');
-const msj = document.querySelector('.msj');
+const form = document.querySelector('.contact_form'),
+    name = document.querySelector('.name'),
+    email = document.querySelector('.email'),
+    asunto = document.querySelector('.asunto'),
+    msj = document.querySelector('.msj');
+
+function sendEmail() {
+    Email.send({
+        SecureToken : "390589f2-c6fe-4f39-83cf-29c747659a13",
+        To : 'maquez.paloma@gmail.com',
+        From : email.value,
+        Subject : 'Contacto desde marciadm.com',
+        Body : msj.value
+    }).then(
+    message => alert(message)
+    );
+}
+
+
