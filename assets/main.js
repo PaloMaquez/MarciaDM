a06975167f4bb20261d482f2235cf2050d0c772d
Paloma Maquez <maquez.paloma@gmail.com>
2022-02-25 22:22:44 -0300

    dnjkcfnsd

---
+++ assets/main.js
@@ -0,0 +1,5 @@
+const form = document.querySelector('.contact_form');
+const name = document.querySelector('.name');
+const email = document.querySelector('.email');
+const asunto = document.querySelector('.asunto');
+const msj = document.querySelector('.msj');
