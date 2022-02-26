32b3150cc04181e89cd037f37bd35c2e786ba7ef
Paloma Maquez <maquez.paloma@gmail.com>
2022-02-26 19:07:18 -0300

    oapap

--- assets/main.js
+++ assets/main.js
@@ -1,19 +1,3 @@
-const form = document.querySelector('.contact_form'),
-    name = document.querySelector('.name'),
-    email = document.querySelector('.email'),
-    asunto = document.querySelector('.asunto'),
-    msj = document.querySelector('.msj');
 
-function sendEmail() {
-    Email.send({
-        SecureToken : "390589f2-c6fe-4f39-83cf-29c747659a13",
-        To : 'maquez.paloma@gmail.com',
-        From : email.value,
-        Subject : 'Contacto desde marciadm.com',
-        Body : msj.value
-    }).then(
-    message => alert(message)
-    );
-}
 
 
