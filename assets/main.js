2b34717ae5893eaa78277301cfe38db9abd85b54
Paloma Maquez <maquez.paloma@gmail.com>
2022-02-27 17:44:39 -0300

    kssks

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,21 @@
+const $form = document.querySelector('#form');
+const thankyou = document.querySelector('.thankyou')
 
+$form.addEventListener('submit', handleSubmit)
 
+async function handleSubmit(event) {
+    event.preventDefault()
+    const form = new FormData (this)
+    const response = await fetch (this.action,{
+        method: this.method,
+        body: form,
+        headers: {
+            'Accept' : 'aplication/json'
+        }
+    })
+    if (response.ok) {
+        this.reset()
+        alert ('Mensaje Enviado');
+    }
+}
 
